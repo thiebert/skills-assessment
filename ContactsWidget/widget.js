9793071046074e55928b12c6da97e245bf43c4ab
Trevin Hiebert <hello@buildatrev.in>
2020-01-05 21:17:41 -0800

    bugfixes
    Fixed several bugs

--- ContactsWidget/widget.js
+++ ContactsWidget/widget.js
@@ -12,19 +12,27 @@ function setInfo() {
 
 function toggleHighlight(index) {
     var blackouts = document.getElementsByClassName('blackout');
-    var isOn = blackouts[0].style.display == "none";
+    var isOn = blackouts[0].style.display == "none" && blackouts[1].style.display == "none";
     for (var i = 0; i < blackouts.length; i++) {
         blackouts[i].style.display = isOn ? "table-cell" : "none";
+        blackouts[i].style["background-color"] = "rgba(24, 24, 24, .8)";
     }
     var rows = document.getElementsByClassName("row");
 
     var phones = document.getElementsByClassName('phone');
     var emails = document.getElementsByClassName('email');
     var infos = document.getElementsByClassName('full-info');
-    phones[index].style.display = isOn ? "none" : "table-cell";
-    emails[index].style.display = isOn ? "none" : "table-cell";
-    infos[index].style.display = isOn ? "table-cell" : "none";
+    for (var i = 0; i < infos.length; i++) {
+        infos[i].style.display = "none";
+    }
+    for (var i = 0; i < rows.length; i++) {
+        rows[i].style["background-color"] = i % 2 ? "#1a1a1a" : "#212121";
+    }
+
+    phones[index + 1].style.display = isOn ? "none" : "table-cell";
+    emails[index + 1].style.display = isOn ? "none" : "table-cell";
+    infos[index + 1].style.display = isOn ? "table-cell" : "none";
     rows[index].style["background-color"] = isOn ? "#444444" : index % 2 ? "#1a1a1a" : "#212121";
-    blackouts[index].style["background-color"] = isOn ? "transparent" : "rgba(24, 24, 24, .8)";
+    blackouts[index + 1].style["background-color"] = isOn ? "transparent" : "rgba(24, 24, 24, .8)";
     if (!isOn) setInfo();
 }
